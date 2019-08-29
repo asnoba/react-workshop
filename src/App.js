33cdfd07a6c59abf5a021be75cf0ae46ce1a20e8
Asger Norskov Bak <anb@mjolner.dk>
2019-08-29 23:18:57 +0200

    React workshop.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <form>
+          <input type="text" />
+          <button type="submit">Tilføj</button>
+        </form>
+        <ul className={styles.list}>
+          <li>
+            <input type="checkbox" /> Køb mælk
+          </li>
+          <li>
+            <input type="checkbox" /> Slå græs
+          </li>
+        </ul>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
